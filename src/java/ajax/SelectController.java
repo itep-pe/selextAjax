063c0bd9ae7a2d396d702e7f61d1cce493f828db
Jefferson Lima <jeff-pe@live.com>
2015-03-24 09:43:11 -0300

    Correção da lógica

--- src/java/ajax/SelectController.java
+++ src/java/ajax/SelectController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ajax;
 
 import java.io.IOException;
@@ -92,8 +87,8 @@ public class SelectController extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         StringBuilder stringBuilder = new StringBuilder("");
-        
-        List<Veiculo> listaCarros = this.consultaCarrosAtividade(codigoAtividade);        
+
+        List<Veiculo> listaCarros = this.consultaCarrosAtividade(codigoAtividade);
 
         for (int i = 0; i < listaCarros.size(); i++) {
             stringBuilder.append(listaCarros.get(i).getCodAtividade() + "-" + listaCarros.get(i).getModelo() + "-" + listaCarros.get(i).getPlaca() + "/");;
